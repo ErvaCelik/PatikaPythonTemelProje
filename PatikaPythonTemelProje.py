0f438b0b1ba7f017e21528d16c4578b8d8201354
ErvaCelik <ervaacelik@gmail.com>
2021-09-02 21:45:40 +0300

    PatikaPythonTemelProje

---
+++ PatikaPythonTemelProje.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Ornek 1
+
+# In[19]:
+
+
+
+ornekListe = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+duzenlenmisListe = []
+
+def flattenListe(liste):
+    
+    for eleman in liste:
+        if type(eleman)== list:
+            flattenListe(eleman)
+        else:
+            duzenlenmisListe.append(eleman)
+    return duzenlenmisListe
+
+flattenListe(ornekListe)
+
+
+# # Ornek 2
+
+# In[20]:
+
+
+
+ornekListe = [[1, 2], [3, 4], [5, 6, 7]]
+
+def reverseListe(liste):
+    
+    for eleman in liste:
+        if type(eleman) == list:
+            reverseListe(eleman)
+    liste.reverse()
+    return liste
+
+reverseListe(ornekListe)
+
